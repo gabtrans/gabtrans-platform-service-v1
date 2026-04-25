6fbdcb63aac3b5384809fb5bdc1d6ff6e8c9fb7f
gbenga <gifalade@gmail.com>
2026-04-25 07:38:10 +0100

    Updated the Globus integration

--- src/GabTrans.Application/obj/Debug/net9.0/GabTrans.Application.AssemblyInfo.cs
+++ src/GabTrans.Application/obj/Debug/net9.0/GabTrans.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GabTrans.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e777a3ef1fe3492318e9af50bf91b8254ed7224")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bccbbc98caba13464a8d1f0b6a435cbea2436724")]
 [assembly: System.Reflection.AssemblyProductAttribute("GabTrans.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GabTrans.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
